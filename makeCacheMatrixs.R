d661ee709b29ad1f6a8883a9366e6ac3b59cc403
monishareddy <67576111+monisha606@users.noreply.github.com>
2020-08-02 14:53:24 +0530

    Update makeCacheMatrixs.R

--- makeCacheMatrixs.R
+++ makeCacheMatrixs.R
@@ -1,23 +1,30 @@
-makeCacheMatrix <- function(x = matrix()) {
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix()) {          #function to take input matrix
   
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
+  inv <- NULL                                          #NULL Object to var inv
+  set <- function(y) {                                 #set value of matrix using function y
+    x <<- y                                  #assignment operator parent function makeCache matrix have acces to this funtion
     inv <<- NULL
   }
-  get <- function() x
-  setinv <- function(inverse) inv <<- inverse
-  getinv <- function() inv
-  list(set = set, get = get, setinv = setinv, getinv = getinv)
+  get <- function() {x}                  #get value of matrix
+  setinv <- function(inverse) inv <<- inverse      #set value of inverse to setInverse
+  getinv <- function() {inv}                       #get value of Inverse
+  list(set = set, get = get, setinv = setinv, getinv = getinv)      #creating list 
 }
+## Write a short comment describing this function
+#Inverse of matrix 
 cacheSolve <- function(x, ...) {
-  inv <- x$getinv()
-  if(!is.null(inv)) {
+   ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()        #returns inverse of matrix and assign to inv
+  if(!is.null(inv)) {      #if already done computation
     message("getting cached result")
     return(inv)
   }
   data <- x$get()
-  inv <- solve(data, ...)
-  x$setinv(inv)
+  inv <- solve(data, ...)           #compute inverse 
+  x$setinv(inv)                          #set value in cache 
   inv
 }
